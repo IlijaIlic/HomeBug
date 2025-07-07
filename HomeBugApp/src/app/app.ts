622f69ee46e6bf946cb403edd7880301ddf8c31d
IlijaIlic <ilic.ilija002@gmail.com>
2025-07-07 18:08:43 +0200

    starting

--- HomeBugApp/src/app/app.ts
+++ HomeBugApp/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Landing } from './landing/landing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Landing],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'HomeBugApp';
+
 }
