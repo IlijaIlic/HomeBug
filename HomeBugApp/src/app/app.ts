442e358b9c4a38786594a2d39677fccd27b2d20b
IlijaIlic <ilic.ilija002@gmail.com>
2025-07-10 15:57:26 +0200

    landing-page-start

--- HomeBugApp/src/app/app.ts
+++ HomeBugApp/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Landing } from './landing/landing';
+import { Header } from './ui-components/header/header';
+import { Footer } from './ui-components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Landing],
+  imports: [RouterOutlet, Landing, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
