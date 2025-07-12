4a82a5f8db429d52b265bed802c5043e18d29e67
IlijaIlic <ilic.ilija002@gmail.com>
2025-07-12 23:17:42 +0200

    add: landingpage-pictures, redesign

--- HomeBugApp/src/app/landing/landing.ts
+++ HomeBugApp/src/app/landing/landing.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { finalize, interval, take } from 'rxjs';
+import { concat, concatMap, finalize, interval, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-landing',
@@ -14,26 +14,26 @@ export class Landing {
   max: number = 1;
   min: number = 19;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
 
-  ngOnInit(){
-    this.http.get('assets/landing-cycle-text.txt', {responseType: "text"}).subscribe(data=>{
+  ngOnInit() {
+    this.http.get('assets/landing-cycle-text.txt', { responseType: "text" }).subscribe(data => {
       this.words = data.split('\n').filter(line => line.trim() !== '');
       this.currWord = this.words[0]
       this.cycleWords();
     });
   }
 
-
-  cycleWords(){
+  cycleWords() {
     interval(5000)
       .pipe(
         take(this.words.length),
-        finalize(()=>this.cycleWords()))
-      .subscribe(()=>{
-        const randValue = Math.floor(Math.random() * (this.max-this.min+1)) + this.min;
+        finalize(() => this.cycleWords())
+      )
+      .subscribe(() => {
+        const randValue = Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
         this.currWord = this.words[randValue];
-      });
+      })
   }
 }
