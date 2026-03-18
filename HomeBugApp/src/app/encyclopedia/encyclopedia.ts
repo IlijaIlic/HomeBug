fb1bf5d18ff1bc53eac57239a69dd5c4fe0c6153
IlijaIlic <ilic.ilija002@gmail.com>
2026-03-18 13:17:28 +0100

    modif: ui-encyclopedia-responsive

--- HomeBugApp/src/app/encyclopedia/encyclopedia.ts
+++ HomeBugApp/src/app/encyclopedia/encyclopedia.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { UnknownBugImage } from '../ui-components/unknown-bug-image/unknown-bug-image';
 import { AppliedFilter } from '../ui-components/applied-filter/applied-filter';
 import { RouterModule } from '@angular/router';
@@ -9,6 +9,31 @@ import { RouterModule } from '@angular/router';
   templateUrl: './encyclopedia.html',
   styleUrl: './encyclopedia.scss',
 })
-export class Encyclopedia {
+export class Encyclopedia implements OnInit {
+
+  width: number = window.innerWidth;
+  size_of_comp: number = 10;
+
+  ngOnInit(): void {
+    if (this.width < 600) {
+      this.size_of_comp = 7;
+    } else {
+      this.size_of_comp = 15
+    }
+
+
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.width = window.innerWidth;
+    if (this.width < 600) {
+      this.size_of_comp = 7;
+    } else {
+      this.size_of_comp = 15
+    }
+
+
+  }
 
 }
