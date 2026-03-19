1b9a23a17905e885c58e43b696d650a8538299a1
IlijaIlic <ilic.ilija002@gmail.com>
2026-03-19 15:05:30 +0100

    modif: ui-responsive: header

--- HomeBugApp/src/app/ui-components/header/header.ts
+++ HomeBugApp/src/app/ui-components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -10,4 +10,22 @@ import { RouterModule } from '@angular/router';
 export class Header {
 
   @Input() wave_show = false;
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    document.body.style.overflow = this.menuOpen ? 'hidden' : ''
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.style.overflow = ''
+
+  }
+
+  @HostListener('document:keydown.escape')
+  handleEsc() {
+    this.closeMenu();
+  }
 }
